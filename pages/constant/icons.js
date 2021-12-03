7327ac2e9a7be9a780a5586e7a805b09ebb47aeb
Pradeep A <sharmapradeep872@gmail.com>
2021-12-03 21:18:49 +0530

    Export styled Icons

---
+++ pages/constant/icons.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { MoonStars } from "@styled-icons/bootstrap";
+import { Sun } from "@styled-icons/fa-solid";
+
+export const MoonStarsIcon = styled(MoonStars)`
+  color: #171923;
+`;
+
+export const SunIcon = styled(Sun)`
+  color: #fff;
+`;
