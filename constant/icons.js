b1ed6ad71543bedf726ea68f144b0dbf46a0eb2f
Pradeep A <sharmapradeep872@gmail.com>
2021-12-04 18:20:27 +0530

    Added new icons

--- constant/icons.js
+++ constant/icons.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { Sun } from "@styled-icons/fa-solid";
-import { MoonStarsFill } from "@styled-icons/bootstrap";
+import { CurrencyExchange, MoonStarsFill } from "@styled-icons/bootstrap";
+import { Convertio } from "@styled-icons/simple-icons";
 
 export const MoonStarsIcon = styled(MoonStarsFill)`
   color: #171923;
@@ -8,4 +9,14 @@ export const MoonStarsIcon = styled(MoonStarsFill)`
 
 export const SunIcon = styled(Sun)`
   color: #fff;
+`;
+
+export const CurrencyExchangeIcon = styled(CurrencyExchange)`
+  color: #171923;
+  dark-mode: #fff;
+`;
+
+export const ConverterIcon = styled(Convertio)`
+  color: #171923;
+  dark-mode: #fff;
 `;
