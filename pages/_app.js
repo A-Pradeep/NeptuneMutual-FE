258bbe1c454dde93c950fffe61a50cbc53fdd51d
Pradeep A <sharmapradeep872@gmail.com>
2021-12-05 03:17:29 +0530

    wrapped all components under `Provider` and added redux store.

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
 import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
   );
 }
 
