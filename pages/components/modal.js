4dd599334b638c4a065cd28863cd6aa7e4b604b8
Pradeep A <sharmapradeep872@gmail.com>
2021-12-09 22:23:23 +0530

    Modal with wallet details or connect/disconnect

--- pages/components/modal.js
+++ pages/components/modal.js
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import {
   Modal,
   ModalOverlay,
@@ -7,9 +8,17 @@ import {
   ModalBody,
   ModalCloseButton,
   Button,
+  Text,
 } from "@chakra-ui/react";
+import { ErrorIcon } from "../../constant/icons";
+import CtaButton from "./ctaButton";
+import WalletTable from "./walletTable";
 
-function CustomModal({ onClose, isOpen }) {
+function CustomModal({ onClose, isOpen, onLogin, onLogout }) {
+  const { walletConnected, isConnecting } = useSelector(
+    (state) => state.wallet
+  );
+
   return (
     <Modal
       closeOnOverlayClick={false}
@@ -21,8 +30,24 @@ function CustomModal({ onClose, isOpen }) {
       <ModalContent>
         <ModalHeader>Wallet Details</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>Modal</ModalBody>
-        <ModalFooter>
+        <ModalBody>
+          {walletConnected && <WalletTable />}
+          {!walletConnected && (
+            <Text color="#F56565">
+              <ErrorIcon size="20px" />
+              Please connect to your wallet to see your wallet details.
+            </Text>
+          )}
+        </ModalBody>
+        <ModalFooter justifyContent="space-evenly">
+          <CtaButton
+            onClick={walletConnected ? onLogout : onLogin}
+            title={walletConnected ? "Disconnect" : "Connect"}
+            bg={walletConnected ? "#C53030" : "#1B4683"}
+            hover_color={walletConnected ? "#C53030" : "#1B4683"}
+            outline={true}
+            isLoading={isConnecting}
+          />
           <Button onClick={onClose}>Close</Button>
         </ModalFooter>
       </ModalContent>
