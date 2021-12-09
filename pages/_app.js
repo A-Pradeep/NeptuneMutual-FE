6f19d33563818f4a7cf1246e383fd02a07a048cf
Pradeep A <sharmapradeep872@gmail.com>
2021-12-09 22:05:48 +0530

    wrapped component under custom wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,15 @@ import { Provider } from "react-redux";
 import { store } from "../redux/store";
 import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
+import WalletProvider from "./walletProvider";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <ChakraProvider>
-        <Component {...pageProps} />
+        <WalletProvider>
+          <Component {...pageProps} />
+        </WalletProvider>
       </ChakraProvider>
     </Provider>
   );
