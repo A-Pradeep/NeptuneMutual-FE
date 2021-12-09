cd0196af9a856fe914b45d4f7af11d5385bb18a5
Pradeep A <sharmapradeep872@gmail.com>
2021-12-09 22:26:11 +0530

    added wallet reducer to store

--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ConverterReducer from "./converter";
+import WalletReducer from "./wallet";
 
 export const store = configureStore({
   reducer: {
     converter: ConverterReducer,
+    wallet: WalletReducer,
   },
 });
