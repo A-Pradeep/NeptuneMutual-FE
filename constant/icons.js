6fc7d7272b97e114e96759188636e72341fe508d
Pradeep A <sharmapradeep872@gmail.com>
2021-12-09 22:01:56 +0530

    Added new icon to the list

--- constant/icons.js
+++ constant/icons.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 import { Sun } from "@styled-icons/fa-solid";
 import { CurrencyExchange, MoonStarsFill } from "@styled-icons/bootstrap";
-import { Convertio } from "@styled-icons/simple-icons";
+import { Convertio, Ethereum } from "@styled-icons/simple-icons";
+import { WindowNew } from "@styled-icons/fluentui-system-filled";
+import { Notification } from "@styled-icons/icomoon";
 
 export const MoonStarsIcon = styled(MoonStarsFill)`
   color: #171923;
@@ -16,5 +18,18 @@ export const CurrencyExchangeIcon = styled(CurrencyExchange)`
 `;
 
 export const ConverterIcon = styled(Convertio)`
+  color: inherit;
+`;
+
+export const NewTabIcon = styled(WindowNew)`
+  color: inherit;
+`;
+
+export const ErrorIcon = styled(Notification)`
+  color: inherit;
+  padding-right: 0.3rem;
+`;
+
+export const EthereumIcon = styled(Ethereum)`
   color: inherit;
 `;
